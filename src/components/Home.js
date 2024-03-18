904c6b5e05f9dd9f8e35a2e100f2fc14b337a280
jusvanth-17 <jusvanth475@gmail.com>
2024-03-18 14:24:46 +0530

    add

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,8 @@ import './css/Marquee.css'
 import './css/Home.css'; 
 import imgb from './img/bandown-2.jpeg'
 import about from './img/iphone__1_-removebg.png'
+import abimg from './img/aboutpage.jpeg'
+import { NavLink } from 'react-router-dom';
 
 export const Home = () => {
   return (
@@ -16,12 +18,15 @@ export const Home = () => {
           </div>
         </header>
      <h2 style={{textAlign:'center',color:'black',fontSize:'30px'}}>About Us</h2>
-     <div><img src={imgb} alt='back'style={{width:'100%',height:'500px'}}/></div>
+     <p style={{fontSize:'20px',fontFamily:'Arial',fontWeight:'bolder'}}>At VMS Mobile, we're passionate about delivering cutting-edge technology to our customers at affordable prices. With a focus on innovation and customer satisfaction, we strive to bring the latest advancements in mobile technology within reach of every individual. Our extensive range of smartphones, tablets, and accessories caters to diverse needs and preferences, ensuring that everyone can find the perfect device to stay connected and productive. Backed by a team of dedicated professionals and a commitment to quality, reliability, and exceptional service, VMS Mobile is your trusted partner in the ever-evolving world of mobile technology."</p>
+     <div><img src={imgb} alt='back'style={{width:'100%',height:'700px'}}/></div>
       <section className='flex-about'> 
       <div className='flex-about1'>
       <h1>Love the Power<br></br><br></br>
           Love the Price
+         
       </h1>
+      <NavLink style={{fontSize:'30px',color:'blue',textDecoration:'none'}} to={'./products'}>BuyNow</NavLink>
       
       </div>
       <div className='flex-about2'>
@@ -32,7 +37,7 @@ export const Home = () => {
             <h2 class="titleText"><span>C</span>ontact Us</h2>
 
         <div class="box">
-            <h4>Send Message</h4>
+            <div><h4>Send Message</h4>
             <div class="input">
                 <input type="text" placeholder="Name"/>
             </div>
@@ -43,8 +48,13 @@ export const Home = () => {
                 <textarea placeholder="message"></textarea>
              </div>
              <div class="input">
-                <input type="submit" placeholder="Sumbit"/>
+                <input style={{backgroundColor:'lightblue',color:'white',borderRadius:'5px'}} type="submit" placeholder="Sumbit"/>
+            </div></div>
+            <div>
+                <img src={abimg} alt='about'/>
             </div>
+
+            
         </div>
     </section>
         <div class="Copyrighttext">
